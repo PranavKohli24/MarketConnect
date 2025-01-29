beec489fa048ea95c35050176b63ce2e97461769
PRANAV KOHLI <pranavkohli@Nehals-MacBook-Pro.local>
2025-01-30 02:07:37 +0530

    message

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,19 +6,11 @@ import {
   setPersistence,
   browserLocalPersistence,
 } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCT_2KGkXwERzgfsB10yOzp5Qg-NlbpyFk",
-  authDomain: "bestbuy1-adb18.firebaseapp.com",
-  projectId: "bestbuy1-adb18",
-  storageBucket: "bestbuy1-adb18.appspot.com",
-  messagingSenderId: "152051881154",
-  appId: "1:152051881154:web:6a2398c8312560cada975b",
-  measurementId: "G-NLN3KBDHVY"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.domain,
+
 };
 
 console.log(process.env);
